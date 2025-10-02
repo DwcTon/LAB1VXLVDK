ee287b71180c404a68dbad047397fa48478847a8
PHAMNGOCDUCTOAN\Đức Toàn <toantom656@gmail.com>
2025-10-03 02:43:19 +0700

    init ex7

--- LAB1VXL/Core/Src/main.c
+++ LAB1VXL/Core/Src/main.c
@@ -62,20 +62,28 @@ static void MX_GPIO_Init(void);
   * @retval int
   */
 
-  uint16_t ledPin[12] = {
+uint16_t ledPin[12] = {
     GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7,
     GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11,
     GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15
 };
-void Led(int counter) {
-    // Tắt tất cả LED
+
+
+void clearAllClock(void) {
     HAL_GPIO_WritePin(GPIOA,
                       GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
                       GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 |
                       GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
                       GPIO_PIN_SET);
+}
 
-    HAL_GPIO_WritePin(GPIOA, ledPin[counter], GPIO_PIN_RESET);
+
+void setAllClock(void) {
+    HAL_GPIO_WritePin(GPIOA,
+                      GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
+                      GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 |
+                      GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
+                      GPIO_PIN_RESET);
 }
 
 int main(void)
@@ -108,14 +116,13 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int counter = 0;
+  	  setAllClock();
+  	  HAL_Delay(2000);
+    clearAllClock();
   while (1)
   {
     /* USER CODE END WHILE */
-	  Led(counter);
-	          counter++;
-	          if (counter >= 12) counter = 0;  // quay vòng
-	          HAL_Delay(1000);
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
